328a9999b343d0d6059ef6656fd28ec782e437c8
xixi zhou <admin@zhouchenxi.cn>
2018-09-14 18:17:38 +0800

    .

---
+++ redis/redis-trib.sh
@@ -0,0 +1,15 @@
+#开一个临时容器初始化redis集群
+kubectl run -i --tty ubuntu --image=ubuntu --restart=Never /bin/bash
+#安装ruby及redis模块
+apt-get update
+apt-get install ruby
+gem sources --add http://gems.ruby-china.com/  --remove https://rubygems.org/
+gem install redis
+
+ruby redis-trib.rb create  --replicas 1 \
+  `dig +short redis-0.redis.default.svc.cluster.local`:6379 \
+  `dig +short redis-1.redis.default.svc.cluster.local`:6379 \
+  `dig +short redis-2.redis.default.svc.cluster.local`:6379 \
+  `dig +short redis-3.redis.default.svc.cluster.local`:6379 \
+  `dig +short redis-4.redis.default.svc.cluster.local`:6379 \
+  `dig +short redis-5.redis.default.svc.cluster.local`:6379 \
