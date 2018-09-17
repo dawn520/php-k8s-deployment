09aa197d392674dcdd19bf3a5259c6db88f30168
xixi zhou <admin@zhouchenxi.cn>
2018-09-17 18:04:44 +0800

    .

--- redis/redis-trib.sh
+++ redis/redis-trib.sh
@@ -3,8 +3,10 @@ kubectl run -i --tty ubuntu --image=ubuntu --restart=Never /bin/bash
 #安装ruby及redis模块
 apt-get update
 apt-get install ruby
+apt-get install dnsutils
 gem sources --add http://gems.ruby-china.com/  --remove https://rubygems.org/
 gem install redis
+wget http://download.redis.io/redis-stable/src/redis-trib.rb
 
 ruby redis-trib.rb create  --replicas 1 \
   `dig +short redis-0.redis.default.svc.cluster.local`:6379 \
